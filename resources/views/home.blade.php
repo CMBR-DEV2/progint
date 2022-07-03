22619d0b9dd5403b2a17f8cf3a6b27c1232f3703
davi marangoni <79322556+davi-marangoni@users.noreply.github.com>
2022-07-03 17:41:46 -0300

    HOmePage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,15 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <div class="card">
+        <h1>Curso de </h1>
+        <p class="title">CEO & Founder, Example</p>
+        <p>Harvard University</p>
+        <p><button>Contact</button></p>
     </div>
+</div>
 </div>
 @endsection
