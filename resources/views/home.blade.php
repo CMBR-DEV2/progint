109f9f91d4d3a40e126698162d979a5d922597d7
davi marangoni <79322556+davi-marangoni@users.noreply.github.com>
2022-07-05 13:53:30 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,15 @@
             <img src="/images/curso.png" class="card-img-top" alt="Imagem do Curso">
             <div class="card-body">
                 <h5 class="card-title">Logica de Programação - 1</h5>
+                <p class="card-text">Neste curso você aprendera a história da programação e como ela é interpretada pelo computador.
+                </p>
+                <a href="cursos.php" class="btn btn-primary">Iniciar Curso</a>
+            </div>
+        </div>
+        <div class="card h-100">
+            <img src="/images/curso.png" class="card-img-top" alt="Imagem do Curso">
+            <div class="card-body">
+                <h5 class="card-title">Logica de Programação - 2</h5>
                 <p class="card-text">Neste curso você aprendera o basico de logica de programação, como utilizar estruturas de decisão e de repetição.
                 </p>
                 <a href="#" class="btn btn-primary">Iniciar Curso</a>
@@ -17,8 +26,17 @@
         <div class="card h-100">
             <img src="/images/curso.png" class="card-img-top" alt="Imagem do Curso">
             <div class="card-body">
-                <h5 class="card-title">Logica de Programação - 1</h5>
-                <p class="card-text">Neste curso você aprendera o basico de logica de programação, como utilizar estruturas de decisão e de repetição.
+                <h5 class="card-title">Logica de Programação - 3</h5>
+                <p class="card-text">Neste curso você aprendera funções de logica de programação, como utilizar estruturas de decisão e de repetição.
+                </p>
+                <a href="#" class="btn btn-primary">Iniciar Curso</a>
+            </div>
+        </div>
+        <div class="card h-100">
+            <img src="/images/curso.png" class="card-img-top" alt="Imagem do Curso">
+            <div class="card-body">
+                <h5 class="card-title">Logica de Programação - 4</h5>
+                <p class="card-text">Neste curso você aprendera o avançado de logica de programação, como utilizar estruturas de decisão e de repetição.
                 </p>
                 <a href="#" class="btn btn-primary">Iniciar Curso</a>
             </div>
